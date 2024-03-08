13a2fcf1acf14cc9b64ab5ab9fd8ef2b8fd458ea
Lars Hoeijmans <i474593@fhict.nl>
2024-03-08 16:39:09 +0100

    Responsiveness home screen
    Massively improved responsiveness of slogan on homescreen (SloganText.vue) by changing 'vh' to 'vw'.

--- src/components/SloganText.vue
+++ src/components/SloganText.vue
@@ -13,7 +13,7 @@
     font-family: "League Spartan", sans-serif;
     font-weight: 900;
     font-style: normal;
-    font-size: 10vh; /* Adjust the font size as needed */
+    font-size: 6vw; /* Adjust the font size as needed */
     color: white; /* Change the color of the slogan */
     margin-top: 9vh; /* Add some space between the image and the slogan */
   }
